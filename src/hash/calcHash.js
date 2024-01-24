fb86720aa713084788f9beb412b0f8b02159072d
SheveleffRoman <@>
2024-01-24 16:24:10 +0300

    feat: add hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+import crypto from "node:crypto";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileToCalculate = await fs.readFile(pathToFile);
+    const hashSum = crypto
+      .createHash("sha256")
+      .update(fileToCalculate)
+      .digest("hex");
+    console.log(hashSum);
+  } catch {
+    throw new Error("Hash operation failed");
+  }
 };
 
 await calculateHash();
