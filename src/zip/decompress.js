ad89feca104da350c8b810dfe7e5a96c5238fd77
SheveleffRoman <@>
2024-01-25 12:41:00 +0300

    feat: add zlib functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+import { createUnzip } from "node:zlib";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const pathToSourceFile = path.join(__dirname, "files", "archive.gz");
+const pathToDestinationFile = path.join(
+  __dirname,
+  "files",
+  "fileToCompress.txt"
+);
+
+const readStream = fs.createReadStream(pathToSourceFile);
+const unzip = createUnzip();
+const writeStream = fs.createWriteStream(pathToDestinationFile);
+
 const decompress = async () => {
-    // Write your code here 
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
